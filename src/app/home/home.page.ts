a5c3079cc84f9e19a7a53cbb98a7eaf38277591b
Tadeu Kaiba <tadeukaiba@gmail.com>
2019-02-20 18:36:28 -0300

    [feat] adicionando upload por camera e galeria

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  public images: string[] = [];
+
+  constructor(private camera: Camera) { }
+
+  public adicionarCamera() {
+    let options : CameraOptions = {
+      quality : 75,
+      destinationType : this.camera.DestinationType.DATA_URL,
+      sourceType : this.camera.PictureSourceType.CAMERA,
+      allowEdit : true,
+      encodingType: this.camera.EncodingType.JPEG,
+      targetWidth: 120,
+      targetHeight: 120,
+      saveToPhotoAlbum: false
+    }
+    this.takePicture(options);
+  }
+
+  public adicionarGaleria() {
+    let options : CameraOptions = {
+      destinationType : this.camera.DestinationType.DATA_URL,
+      sourceType : this.camera.PictureSourceType.PHOTOLIBRARY,
+      encodingType: this.camera.EncodingType.JPEG,
+      allowEdit : true,
+      targetWidth: 120,
+      targetHeight: 120,
+    }
+    this.takePicture(options);
+  }
+
+  private takePicture(options:CameraOptions) {
+    // const options: CameraOptions = {
+    //   quality: 100,
+    //   destinationType: this.camera.DestinationType.FILE_URI,
+    //   encodingType: this.camera.EncodingType.JPEG,
+    //   mediaType: this.camera.MediaType.PICTURE
+    // }
+    
+    this.camera.getPicture(options).then((imageData) => {
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.images.push(base64Image);
+    }, (err) => {
+      console.log(err);
+    });
+  }
 
 }
